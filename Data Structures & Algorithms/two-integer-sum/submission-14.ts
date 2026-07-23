9384f2fcfefbecc5dc1cf6cfd2506ea76b6f67cb
Nicolas <60152656+byNico1@users.noreply.github.com>
2026-07-22 22:40:30 -0500

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const result = [];
+        const numsMap = {};
+
+        for (const column in nums) {
+            let rest = target - nums[column];
+            if (numsMap[rest] === undefined) {
+                let columnValue = nums[column]
+                numsMap[columnValue] = column;
+            } else {
+                result.push(Number(numsMap[rest]), Number(column));
+                return result.sort((a, b) => a - b);
+            }
+        }
+    }
+}
