817136c01a07191b502b7eeeb2147d04dfc4b401
Nicolas <60152656+byNico1@users.noreply.github.com>
2026-07-26 19:43:14 -0500

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums: number[], val: number): number {
+        let k = 0;
+
+        for (let num in nums) {
+            if (nums[num] !== val) {
+                nums[k] = nums[num];
+                k += 1;
+            }
+        }
+        
+        return k;
+    }
+}
