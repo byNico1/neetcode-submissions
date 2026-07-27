799a25e5aa9ff04d752cb02e26ad65409c6bb05f
Nicolas <60152656+byNico1@users.noreply.github.com>
2026-07-27 15:24:41 -0500

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums: number[]): number {
+        let counter = 0
+        let result
+
+        for (let num of nums) {
+            if (result === undefined || counter === 0) result = num
+
+            if (num !== result) {
+                counter -= 1;
+            } else {
+
+            counter += 1
+            }
+
+        }
+
+        console.log(counter)
+        console.log(result)
+
+        return result
+    }
+}
