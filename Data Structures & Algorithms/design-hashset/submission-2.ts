11ae812f9021e6b79560f4b8f3832d551c326053
Nicolas <60152656+byNico1@users.noreply.github.com>
2026-07-30 14:04:43 -0500

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.ts
@@ -0,0 +1,74 @@
+class ListNode {
+    key: number;
+    next: ListNode | null;
+
+    constructor(key: number) {
+        this.key = key;
+        this.next = null;
+    }
+}
+
+class MyHashSet {
+    private set: ListNode[];
+
+    constructor() {
+        this.set = Array.from({ length: 10000 }, () => new ListNode(0));
+    }
+
+    hash(key: number) {
+        return key % this.set.length;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key: number): void {
+        let index = this.set[this.hash(key)];
+        while (index.next) {
+            if (index.next.key === key) return;
+
+            index = index.next;
+        }
+        index.next = new ListNode(key);
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key: number): void {
+        let index = this.set[this.hash(key)];
+        while (index.next) {
+            if (index.next.key === key) {
+                index.next = index.next.next;
+                return;
+            }
+
+            index = index.next;
+        }
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key: number): boolean {
+        let index = this.set[this.hash(key)];
+        while (index.next) {
+            if (index.next.key === key) return true;
+
+            index = index.next;
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
