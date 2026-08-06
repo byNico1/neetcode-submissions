079ee1615742e0bfa58fff43650e636b43b4a7f2
Nicolas <60152656+byNico1@users.noreply.github.com>
2026-08-06 11:30:21 -0500

    Add: string-encode-and-decode - submission-35

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-35.ts
@@ -0,0 +1,59 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let result = "";
+
+        for (let str of strs) {
+            result += str.length + "-";
+
+            str.split("").forEach((char) => {
+                result += String.fromCharCode(char.charCodeAt(0) - "A".charCodeAt(0));
+            });
+        }
+
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        let result = [];
+        let i = 0;
+
+        while (i < str.length) {
+            let j = i;
+            while (str[j] !== "-") {
+                j++;
+            }
+            let length = parseInt(str.substring(i, j));
+            let decodedWord = [];
+            let word = str.substring(j + 1, j + 1 + length);
+
+            word.split("").forEach((char) =>
+                decodedWord.push(String.fromCharCode(char.charCodeAt(0) + "A".charCodeAt(0))),
+            );
+
+            result.push(decodedWord.join(""));
+            i = j + 1 + length;
+        }
+
+        // let words = str.split("-");
+
+        // for (let word of words) {
+        //     let decodedWord = [];
+
+        //     word.split("").forEach((char) =>
+        //         decodedWord.push(String.fromCharCode(char.charCodeAt(0) + "A".charCodeAt(0))),
+        //     );
+
+        //     result.push(decodedWord.join(""));
+        // }
+
+        return result;
+    }
+}
