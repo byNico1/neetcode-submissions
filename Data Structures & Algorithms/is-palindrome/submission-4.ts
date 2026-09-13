d9095746095d00fa85e1d31ea02693fea36b9c2e
Nicolas <60152656+byNico1@users.noreply.github.com>
2026-09-12 19:00:10 -0500

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let backwards = s
+            .replace(/[^a-zA-Z0-9]/g, "")
+            .split("")
+            .reverse()
+            .join("")
+            .toLowerCase();
+        s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+        if (backwards === s) return true;
+
+        return false;
+    }
+}
